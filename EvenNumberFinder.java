7c8be6476409c5fe436e1b192983badfd5104bb4
Karahan Yavuz Kara <64289535+karahanyavuzkara@users.noreply.github.com>
2022-12-10 16:44:05 +0300

    Add files via upload

---
+++ EvenNumberFinder.java
@@ -0,0 +1,19 @@
+package patika;
+import java.util.Scanner;
+public class EvenNumberFinder {
+
+public static void main (String[]args) {
+
+	int k ; 
+    Scanner input = new Scanner(System.in);
+      System.out.print("Enter a number : ");
+   k = input.nextInt();
+
+  for (int i = 1 ; i <= k ; i++ ) {
+	  
+	  if(i % 2 == 0  ) {
+	  System.out.println(i);
+  }
+  }
+ }
+}
